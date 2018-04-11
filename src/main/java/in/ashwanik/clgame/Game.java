ceda5ecb35ba1747778ff8237a17d401ecf8848c
Ashwani Kumar <ashwanikumar04@gmail.com>
2018-04-11 13:43:37 +0530

    added project base

---
+++ src/main/java/in/ashwanik/clgame/Game.java
@@ -0,0 +1,12 @@
+package in.ashwanik.clgame;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Game {
+
+    public static void main(String[] args) {
+        log.info("test");
+    }
+}
